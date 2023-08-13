0210349f89c09ad92aff099c2134acba673e5f24
Sukesh Reddy <117748776+Sukesh79@users.noreply.github.com>
2023-08-13 22:14:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function decimalToBinary(num) {
   //Write you code here
-  
-}
+	let str = "";
+	let n = num;
 
-window.decimalToBinary = decimalToBinary;
+	while(n > 1){
+		let remain = n % 2;
+		str += remain;
+		n = Math.floor(n/2);
+	}
+
+	if(n===1) str += "1";
+	let ans = [...str].reverse().join("");
+	alert(ans);
+}
+let num = parseInt(prompt("enter the number in decimal","10"))
+
+window.decimalToBinary = decimalToBinary(num);
